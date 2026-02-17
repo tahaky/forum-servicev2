ecdf0cdb3b1ff277d7e7189bde5c934b9abf2fa7
thkya <tahakaya36@hotmail.com>
2026-02-17 16:54:54 +0300

    fix

--- src/main/java/com/forum/entity/Subthread.java
+++ src/main/java/com/forum/entity/Subthread.java
@@ -24,7 +24,7 @@ public class Subthread {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false,columnDefinition = "TEXT")
     private String initalMessage;
 
     @Column(name = "created_at", nullable = false)
